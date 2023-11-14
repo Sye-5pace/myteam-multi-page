dab5fcfe46373472504176d1181ce4905923daca
Sye-5pace <owusilasyp00@gmail.com>
2023-11-14 11:34:22 +0000

    :sparkles: Feat: Form validation with visual feedback created

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  // styleUrls: ['./contact.component.scss']
 })
+
+
 export class ContactComponent {
+  contactForm: FormGroup;
+  emailRegex: string = '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}';
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      fullname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email, Validators.pattern(this.emailRegex)]],
+      company: ['', Validators.required],
+      title: ['', Validators.required],
+      message: ['', Validators.required]
+    })
+  }
+
+
+  onSubmit(){
+    if (this.contactForm.value){
+      console.log(this.contactForm.value)
+      this.contactForm.reset()
+    }
+  }
 
 }
